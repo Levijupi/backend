349704be65c5b7ded2486443e8d6f9f8150bf71f
LM <levijupi88@gmail.com>
2025-04-30 17:27:12 -0300

    Tentar ler a url

--- src/main/java/com/barbearia/backend/BackendApplication.java
+++ src/main/java/com/barbearia/backend/BackendApplication.java
@@ -7,5 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        
+        System.out.println("URL: " + System.getenv("DB_URL"));
+		System.out.println("USER: " + System.getenv("DB_USER"));
+		System.out.println("PASS: " + System.getenv("DB_PASS"));
     }
 }
